ddbcae739f5260dadcde47a5fb33a6bcf8b8d0b8
José Boné <79787418+jose-bone@users.noreply.github.com>
2022-05-05 20:41:53 +0200

    Comunicação de componentes 👨🏾‍💻📝

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -9,7 +9,7 @@ import ideaImageUrl from "../../assets/idea.svg";
 import thoughtImageUrl from "../../assets/thought.svg";
 import { FeedbackTypeStep } from "../Steps/FeedbackTypeStep";
 
-const feedbackTypes = {
+export const feedbackTypes = {
   BUG: {
     title: "Problema",
     image: {
@@ -38,6 +38,10 @@ export type FeedbackType = keyof typeof feedbackTypes;
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
 
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+  }
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       <header>
@@ -49,7 +53,10 @@ export function WidgetForm() {
       {!feedbackType ? (
         <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
       ) : (
-        <p>Hello World!</p>
+        <FeedbackContentStep
+          feedbackType={feedbackType}
+          onFeedbackRestartRequested={handleRestartFeedback}
+        />
       )}
 
       <footer className="text-xs text-neutral-400">

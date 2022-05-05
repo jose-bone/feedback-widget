40de8d3ada2803f3441846dc4086d015c18e508c
José Boné <79787418+jose-bone@users.noreply.github.com>
2022-05-05 23:27:11 +0200

    Conteúdo do feedback 📝🧪

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -11,6 +11,8 @@ export function FeedbackContentStep({
   feedbackType,
   onFeedbackRestartRequested,
 }: FeedbackContentStepProps) {
+  const [screenshot, setScreenshot] = useState<string | null>(null);
+
   const feedbackTypeInfo = feedbackTypes[feedbackType];
 
   return (
@@ -36,7 +38,26 @@ export function FeedbackContentStep({
         <CloseButton />
       </header>
 
-      <div className="flex py-8 gap-2 w-full"></div>
+      <form className="my-4 w-full" action="">
+        <textarea
+          className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+          placeholder="Conte com detalhes o que está acontecendo..."
+        />
+
+        <footer className="flex gap-2 mt-2">
+          <ScreenshotButton
+            screenshot={screenshot}
+            onScreenshotTook={setScreenshot}
+          />
+
+          <button
+            type="submit"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+          >
+            Enviar feedback
+          </button>
+        </footer>
+      </form>
     </>
   );
 }

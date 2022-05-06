aae712ee7bba021b99131121d5df78a5eb0fcaba
José Boné <79787418+jose-bone@users.noreply.github.com>
2022-05-06 20:03:11 +0200

    Envio de e-mail 📧

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,15 @@ const app = express();
 
 app.use(express.json());
 
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "08c23f176c4fd4",
+    pass: "2ca9ca6abf7100",
+  },
+});
+
 app.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
 
@@ -13,6 +22,18 @@ app.post("/feedbacks", async (req, res) => {
       comment,
       screenshot,
     },
+  });
+
+  await transport.sendMail({
+    from: "Equipa Feedget <oi@feedget.com>",
+    to: "José Boné <bjosemateus@gmail.com>",
+    subject: "Novo feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size:16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`,
+    ].join("\n"),
   });
 
   return res.status(201).json({ data: feedback });
